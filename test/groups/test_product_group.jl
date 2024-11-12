5172b3bdaa86b6b16b199766e8435ab65d14aa00
Ronny Bergmann <git@ronnybergmann.net>
2024-11-12 17:41:04 +0100

    increase test coverage.

--- test/groups/test_product_group.jl
+++ test/groups/test_product_group.jl
@@ -37,4 +37,12 @@ using LieGroupsTestSuite
         :repr => "ProductLieGroup(LieGroupsTestSuite.DummyManifold() × LieGroupsTestSuite.DummyManifold(), LieGroupsTestSuite.DummyOperation() × LieGroupsTestSuite.DummyOperation())",
     )
     test_lie_group(G2, properties, expectations)
+
+    @testset "Product Operation generators" begin
+        op2 = LieGroupsTestSuite.DummySecondOperation()
+        O1 = op × op2
+        O2 = op2 × op
+        @test (O1 × op) == (op × O2)
+        @test (O1 × O2) == (op × op2 × op2 × op)
+    end
 end
