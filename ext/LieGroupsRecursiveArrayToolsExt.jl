7ded5e9b7aa8de00fb923d0a13e92e6ffb50c083
Ronny Bergmann <git@ronnybergmann.net>
2025-01-11 11:34:37 +0100

    Start the extension for SE(n) on ArrayPartitions.

---
+++ ext/LieGroupsRecursiveArrayToolsExt.jl
@@ -0,0 +1,9 @@
+module LieGroupsRecursiveArrayToolsExt
+
+using LieGroups
+using RecursiveArrayTools: ArrayPartition
+using StaticArrays
+using LinearAlgebra
+
+    # Implement SE(n) also on an Array Partition
+end
