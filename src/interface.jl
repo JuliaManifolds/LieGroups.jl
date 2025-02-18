30156fa5de63b6a4efb5fb8bb9c7aa998ad43cef
Ronny Bergmann <git@ronnybergmann.net>
2025-02-18 09:26:16 +0100

    Fix a typo in the interface docs.

--- src/interface.jl
+++ src/interface.jl
@@ -91,7 +91,7 @@ While an points and tangent vectors are usually kept untyped for flexibility,
 it might be necessary to distinguish different types of points, for example
 
 * for complicated representations that require a struct
-@ semantic verification
+* semantic verification
 * when there exist different representations
 
 By sub-typing the [`AbstractManifoldPoint`](@extref `ManifoldsBase.AbstractManifoldPoint`), this follows the same idea as in $(_link(:ManifoldsBase)).
