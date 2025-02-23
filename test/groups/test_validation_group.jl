fccfc5f68ce5fb34fa34b25ab266a755c1513bba
Ronny Bergmann <git@ronnybergmann.net>
2025-02-23 16:41:13 +0100

    add (dummy for now) test file.

---
+++ test/groups/test_validation_group.jl
@@ -0,0 +1,9 @@
+using LieGroups, Random, Test
+
+s = joinpath(@__DIR__, "..", "LieGroupsTestSuite.jl")
+!(s in LOAD_PATH) && (push!(LOAD_PATH, s))
+using LieGroupsTestSuite
+
+@testset "Validation Lie group" begin
+
+end
