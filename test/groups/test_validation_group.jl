b784fc10fab9040dfd49fbcc1b29384f884e52ee
Ronny Bergmann <git@ronnybergmann.net>
2025-02-23 17:05:28 +0100

    runs formatter.

--- test/groups/test_validation_group.jl
+++ test/groups/test_validation_group.jl
@@ -4,5 +4,4 @@ s = joinpath(@__DIR__, "..", "LieGroupsTestSuite.jl")
 !(s in LOAD_PATH) && (push!(LOAD_PATH, s))
 using LieGroupsTestSuite
 
-@testset "Validation Lie group" begin
-end
+@testset "Validation Lie group" begin end
