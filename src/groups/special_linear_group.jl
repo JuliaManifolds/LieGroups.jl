3138bd44410a06c02efb9f3a55a4cabf941962a2
Ronny Bergmann <git@ronnybergmann.net>
2025-04-02 19:17:27 +0200

    Fix / finish docs.

--- src/groups/special_linear_group.jl
+++ src/groups/special_linear_group.jl
@@ -14,7 +14,7 @@ for ``p ∈ $(_math(:SL))(n,𝔽)`` are represented with their corresponding Lie
     GeneralLinearGroup(n::Int, field=ℝ; kwargs...)
 
 Generate the general linear group  group on ``𝔽^{n×n}``.
-All keyword arguments in `kwargs...` are passed on to [`InvertibleMatrices`](@extref `Manifolds.GeneralUnitaryMatrices`)`{T, 𝔽,`[`DeterminantOneMatrices`](@extref `Manifolds.DeterminantOneMatrices`)`}`.
+All keyword arguments in `kwargs...` are passed on to [`DeterminantOneMatrices`](@extref `Manifolds.DeterminantOneMatrices`).
 """
 const SpecialLinearGroup{𝔽,T} = LieGroup{
     𝔽,MatrixMultiplicationGroupOperation,DeterminantOneMatrices{𝔽,T}
@@ -51,6 +51,28 @@ function get_vector_lie!(
     return X
 end
 
+_doc_hat_special_linear = """
+    X = hat(𝔤::LieAlgebra{ℝ,MatrixMultiplicationGroupOperation,<:SpecialLinearGroup}, c)
+    hat!(𝔤::LieAlgebra{ℝ,MatrixMultiplicationGroupOperation,<:SpecialLinearGroup}, X, c)
+
+Compute the hat map ``(⋅)^̂ : ℝ^{n^2-1} → 𝔤`` that turns a vector of coordinates `c``
+into a tangent vector in the Lie algebra.
+
+The formula on the Lie algebra ``𝔤`` of the [`SpecialLinearGroup`](@ref)`(n)` is given by
+reshaping ``c ∈ ℝ^{n^2-1}`` into an ``n``-by``n``matrix ``X`` with the final entry `X[n,n]`
+initialised to zero and then set to the trace of this initial matrix.
+
+This can be computed in-place of `X`.
+"""
+
+@doc "$(_doc_hat_special_linear)"
+ManifoldsBase.hat(::LieAlgebra{ℝ,MatrixMultiplicationGroupOperation,<:SpecialLinearGroup}, c)
+
+@doc "$(_doc_hat_special_linear)"
+ManifoldsBase.hat!(
+    ::LieAlgebra{ℝ,MatrixMultiplicationGroupOperation,<:SpecialLinearGroup}, X, c
+)
+
 function Base.show(
     io::IO, ::SpecialLinearGroup{𝔽,ManifoldsBase.TypeParameter{Tuple{n}}}
 ) where {𝔽,n}
@@ -61,3 +83,27 @@ function Base.show(io::IO, G::SpecialLinearGroup{𝔽,Tuple{Int}}) where {𝔽}
     n = ManifoldsBase.get_parameter(M.size)[1]
     return print(io, "SpecialLinearGroup($n, $(𝔽); parameter=:field)")
 end
+
+_doc_vee_special_linear = """
+    c = vee(𝔤::LieAlgebra{ℝ,MatrixMultiplicationGroupOperation,<:SpecialLinearGroup}, X)
+    vee!(𝔤::LieAlgebra{ℝ,MatrixMultiplicationGroupOperation,<:SpecialLinearGroup}, c, X)
+
+Compute the vee map ``(⋅)^∨: $(_math(:𝔤)) →  ℝ^{n^2-1}`` that maps a tangent vector
+from the Lie algebra to a vector of coordinates `c``
+
+The formula on the Lie algebra ``𝔤`` of the [`SpecialLinearGroup`](@ref)`(n)` is given by
+reshaping ``X ∈ ℝ^{n×n}`` into a vector and omitting the last entry, since that
+can be reconstructed by considering that ``X`` has to be of trace zero.
+
+This can be computed in-place of `c`.
+"""
+
+@doc "$(_doc_vee_special_linear)"
+ManifoldsBase.vee(
+    ::LieAlgebra{ℝ,MatrixMultiplicationGroupOperation,<:SpecialLinearGroup}, X
+)
+
+@doc "$(_doc_vee_special_linear)"
+ManifoldsBase.vee!(
+    ::LieAlgebra{ℝ,MatrixMultiplicationGroupOperation,<:SpecialLinearGroup}, c, X
+)
