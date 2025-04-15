fb143e95cb467abbb60115c6fb810684f0892316
plc99 <paula_conrad@hotmail.de>
2025-04-15 19:08:25 +0200

    Add documentation
    *for diff_left_inv, diff_right_ins and log for planar circle group

--- src/groups/circle_group_sphere.jl
+++ src/groups/circle_group_sphere.jl
@@ -19,8 +19,34 @@ function diff_conjugate(::_PlanarCircleGroup, g, h, X)
     return X
 end
 
+_doc_diff_left_compose_mult_planar_circ = """
+    diff_left_compose(G::LieGroup{ℝ, AbelianMultiplicationGroupOperation, Sphere}, g, h, X)
+    diff_left_compose!(G::LieGroup{ℝ, AbelianMultiplicationGroupOperation, Sphere}, Y, g, h, X)
+
+Compute the differential of the left group multiplication ``λ_g(h) = g$(_math(:∘))h`` of the Circle Group,
+represented as two dimensional vectors in ``ℝ^2``.
+
+It simplifies for the [`AbelianMultiplicationGroupOperation`](@ref) to ``Dλ_g(h)[X] = gX``,
+where the multiplication corresponds to the complex multiplication after canonical identification
+of the real plane with the complex plane.
+"""
+
+@doc "$(_doc_diff_left_compose_mult_planar_circ)"
 diff_left_compose(C::_PlanarCircleGroup, g, h, X) = compose(C, g, X)
 
+_doc_diff_right_compose_mult_planar_circ = """
+    diff_right_compose(G::LieGroup{ℝ, AbelianMultiplicationGroupOperation, Sphere}, g, h, X)
+    diff_right_compose!(G::LieGroup{ℝ, AbelianMultiplicationGroupOperation, Sphere}, Y, g, h, X)
+
+Compute the differential of the right group multiplication ``λ_g(h) = g$(_math(:∘))h`` of the Circle Group,
+represented as two dimensional vectors in ``ℝ^2``.
+
+It simplifies for the [`AbelianMultiplicationGroupOperation`](@ref) to ``Dλ_g(h)[X] = Xg``,
+where the multiplication corresponds to the complex multiplication after canonical identification
+of the real plane with the complex plane.
+"""
+
+@doc "$(_doc_diff_right_compose_mult_planar_circ)"
 diff_right_compose(C::_PlanarCircleGroup, g, h, X) = compose(C, X, g)
 
 _doc_exp_planar_circ = """
@@ -32,9 +58,6 @@ It coincides with the [ordinary complex exponential](https://en.wikipedia.org/wi
 canonical identification of the real plane with the complex plane.
 
 This can be computed in-place of `g`.
-```math
-$(_tex(:exp)) (t) = $(_tex(:pmatrix, _tex(:cos)*"(t)", _tex(:sin)*"(t)"))
-```
 """
 @doc "$(_doc_exp_planar_circ)"
 function Base.exp(::_PlanarCircleGroup, X)
@@ -44,6 +67,7 @@ end
 
 @doc "$(_doc_exp_planar_circ)"
 ManifoldsBase.exp!(G::_PlanarCircleGroup, g, X) = copyto!(g, exp(G, X))
+
 function _compose(::_PlanarCircleGroup, p, q)
     a = p[1]
     b = p[2]
@@ -71,6 +95,22 @@ function inv_right_compose(C::_PlanarCircleGroup, g, h)
     h1 = Base.inv(C, h)
     return compose(C, g, h1)
 end
+
+_doc_log_planar_circ = """
+    log(::LieGroup{ℝ, AbelianMultiplicationGroupOperation, Sphere}, g)
+    log!(::LieGroup{ℝ, AbelianMultiplicationGroupOperation, Sphere}, X, g)
+
+Compute the Lie group logarithm on the [`CircleGroup`](@ref), , represented as two dimensional vectors in the real plane.
+It coincides with the ordinary complex logarithm after canonical identification of the real plane with the complex plane.
+
+This can be computed in-place of `X`.
+"""
+
+@doc "$(_doc_log_planar_circ)"
+ManifoldsBase.log(::_PlanarCircleGroup, g)
+
+@doc "$(_doc_log_planar_circ)"
+ManifoldsBase.log!(M::_PlanarCircleGroup, X, g)
 
 function ManifoldsBase.log(::_PlanarCircleGroup, g)
     z = log(g[1] + g[2]*im)
